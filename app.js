04603e69eb57a43a34cb899d12455088c14b08d1
wjunxxx <wjun@88.com>
2023-02-27 23:42:30 +0800

    开启静态资源访问

--- app.js
+++ app.js
@@ -4,65 +4,41 @@ const multer = require('multer')
 const app = express()
 const port = 3000
 
-var createFolder = function (folder) {
-    try {
-        fs.accessSync(folder);
-    } catch (e) {
-        fs.mkdirSync(folder);
-    }
-};
+/* 开启静态资源访问 */
+app.use(express.static('upload'));
 
-var uploadFolder = './upload/';
-
-createFolder(uploadFolder);
-
-// 通过 filename 属性定制
+// 配置文件上传
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
-    },
-    filename: function (req, file, cb) {
-        // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
-        cb(null, file.fieldname + '-' + Date.now());
-    }
+	destination: function (req, file, cb) {
+		cb(null, './upload/image/')
+	},
+	filename: function (req, file, cb) {
+		let { originalname } = file;
+		const after = originalname.split('.')[1] ? '.' + originalname.split('.')[1] : '';
+		cb(null, Date.now() + after);
+	}
 });
-
-// const upload = multer({ dest: 'upload/' }); //定义文件的保存路径
-const upload = multer({ storage: storage }) //自定文件保存路径
+const upload = multer({ storage: storage })
 
 app.get('/', (req, res) => {
-    res.send('上传测试')
+	res.send('上传测试')
 })
-// 单图上传
-// app.post('/upload', upload.single('logo'), function (req, res, next) {
-//     var file = req.file;
-
-//     console.log('文件类型：%s', file.mimetype);
-//     console.log('原始文件名：%s', file.originalname);
-//     console.log('文件大小：%s', file.size);
-//     console.log('文件保存路径：%s', file.path);
-
-//     res.send({ ret_code: '200', message: `${file.originalname}上传成功` });
-// });
 
 // 多图上传
-app.post('/upload', upload.array('logo', 999), function (req, res, next) {
-    console.log(req.files);
-
-    // let data = [];
-    // req.forEach(element => {
-    //     let url = 'http://localhost:3000/' + element.path
-    //     data.push(url)
-    // });
-
-    res.send({ ret_code: '0' });
+app.post('/uploadFile', upload.array('logo', 999), function (req, res, next) {
+	console.log(req.files);
+	let url = `http://localhost:3000/image/${req.files[0].filename}`
+	res.send({ code: '200', url });
 });
 
+
+// 前端上传页面
 app.get('/form', function (req, res, next) {
-    var form = fs.readFileSync('./form.html', { encoding: 'utf8' });
-    res.send(form);
+	var form = fs.readFileSync('./form.html', { encoding: 'utf8' });
+	res.send(form);
 });
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
